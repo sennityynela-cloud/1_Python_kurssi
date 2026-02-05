4561a42038b58c7e81536e7ac4e829dfbb5a41e2
senni <senni.tyynela@metropolia.fi>
2026-02-05 22:57:37 +0200

    toinen moduuli

--- moduuli2.2..py
+++ moduuli2.2..py
@@ -1,4 +1,4 @@
 import math
 sade = float(input("Anna ympyrän säde: "))
-pinta-ala = math.pi * sade ** 2
-print("Ympyrän pinta-ala on:", pinta-ala)
+pintaala = math.pi * sade ** 2
+print("Ympyrän pinta-ala on:", pintaala)
