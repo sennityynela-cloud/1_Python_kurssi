6fcb036d84b23e14e18313e4bba271fcc3d499e5
senni <senni.tyynela@metropolia.fi>
2026-02-07 11:01:33 +0200

    toinen moduuli

---
+++ moduuli2.4.py
@@ -0,0 +1,8 @@
+import math
+
+luku1=int(input("Anna luku:"))
+luku2=int(input("Anna toinen luku:"))
+luku3=int(input("Anna kolmas luku:"))
+print(luku1+luku2+luku3)
+print(luku1*luku2*luku3)
+print((luku1+luku2+luku3)/3)
