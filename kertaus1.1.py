1015ec4d6c6c987e89cb3987fc9d571497fab2a0
senni <senni.tyynela@metropolia.fi>
2026-03-04 21:13:57 +0200

    kertaus

--- kertaus1.1.py
+++ kertaus1.1.py
@@ -1,4 +1,7 @@
-nimi=str(input("Mikä on nimesi?"))
-if nimi != "Matti".lower():
-    keittoannos=int(input("Mikä on keittoannosten määrä?"))
-    print(f"Kokonaishinta on",keittoannos*5.9,"euroa")
+while True:
+    nimi=str(input("Mikä on nimesi?"))
+    if nimi != "Matti".lower():
+        keittoannos=int(input("Mikä on keittoannosten määrä?"))
+        print(f"Kokonaishinta on",keittoannos*5.9,"euroa")
+    else:
+        print("Kiitos, hei!")
