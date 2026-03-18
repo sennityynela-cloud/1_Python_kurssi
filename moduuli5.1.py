e3d5b04527ef41ed6aee40b1a65b8dddecbcfa92
senni <senni.tyynela@metropolia.fi>
2026-03-18 18:50:31 +0200

    viides moduuli

--- moduuli5.1.py
+++ moduuli5.1.py
@@ -4,4 +4,4 @@ montako=int(input("Montako arpakuutiota on?"))
 for i in range(montako):
     kuutio = random.randint(1, 6)
     summa=summa+kuutio
-    print(f"Arpakuution silmäluvun summa on", kuutio)
+    print(f"Arpakuution silmäluvun summa on", summa)
