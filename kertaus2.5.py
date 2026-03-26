a46ee6de6b4009798d18c3f7d890c8dc7cf20cc9
senni <senni.tyynela@metropolia.fi>
2026-03-26 22:03:32 +0200

    kertaus 2

---
+++ kertaus2.5.py
@@ -0,0 +1,7 @@
+def suurin_arvo():
+    arvo1=int(input("Anna ensimmäinen arvo: "))
+    arvo2=int(input("Anna toinen arvo: "))
+    arvo3=int(input("Anna viimeinen arvo: "))
+    print("Suurin arvo on: ",max(arvo1,arvo2,arvo3))
+
+suurin_arvo()
